04625766e564ee536257476b10cb19e747446d3b
Rui Wu <rwu4@e5-05-05.ece.ualberta.ca>
2018-02-16 16:02:47 -0700

    optimize

--- lab3_test.c
+++ lab3_test.c
@@ -32,13 +32,83 @@ int main(int argc, char const *argv[])
 	GET_TIME(start);
 	X = calloc(N, sizeof(double));
 
-	//#pragma omp parallel num_threads(p)
+	//# pragma omp parallel num_threads(p)
 	//{
-	Gaussian(NULL);
-	Jordan(NULL);
+		for(int k = 0; k< N-1; k++){
+		// eliminate elements below the diagonal to zero one column after another
+		// Pivoting
+		// In U, from row k to row n, find the row kp, that has the maximum absolute value
+		//int kp = findAbsoluteMax(G, k, N);
+		//# pragma omp parallel num_threads(p)
+		//{
+			int kp = 0;
+			double maxAbsolute = 0;
+
+			# pragma omp parallel num_threads(p)
+			{
+				# pragma omp for
+				for(int i = k; i<N; i++){
+					#pragma omp critical
+					{
+						if(fabs(G[i][k]) >= maxAbsolute){
+					//# pragma omp critical
+					//{
+							maxAbsolute = fabs(G[i][k]);
+							kp = i;
+						}
+					}
+				}
+			
+			
+	//}
+			//Swap row k and row kp
+			//swapRow(k, kp);
+			# pragma omp critical
+			{
+				double *temp = G[k];
+				G[k] = G[kp];
+				G[kp] = temp;
+			}
+			/*Elimination*/
+	
+			//OPEN MP HERE
+			//# pragma omp parallel num_threads(p)
+			//{
+			# pragma omp barrier
+			# pragma omp for
+			for(int i = k+1; i< N; i++){
+				double temp = G[i][k] / G[k][k];
+				for(int j = k; j<N+1; j++){
+					G[i][j] = G[i][j] - temp * G[k][j]; /* Row replacement */
+				}
+			}
+		}
+	}
 
 	# pragma omp parallel num_threads(p)
 	{
+
+		//int i= 0;
+		//int k=0;
+
+		for(int k = N-1; k > 0 ; k--){
+			//# pragma omp parallel num_threads(p)
+			//{
+			# pragma omp for
+		//Eliminate elements to zero for each column one after another
+			for(int i = 0; i < k; i++){
+				//Row replacement one row after another
+				//Open MP HERE
+				G[i][N] = G[i][N] - G[i][k]/G[k][k] * G[k][N];
+				G[i][k] = 0;
+	
+			}
+		}
+	
+
+	//Gaussian(NULL);
+	//Jordan(NULL);
+
 		# pragma omp for
 		for(int i = 0; i < N; i++){
 			//open MP here
